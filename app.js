cf55f75c81d4a7929521a5c0d7662c2800962ce7
sharkwedy <talls.urin@hotmail.com>
2018-10-03 20:56:50 -0300

    d

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +23,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+require('./auth')(passport);
+app.use(session({
+  store: new MongoStore({
+    db: global.db,
+    ttl: 30 * 60 // = 30 minutos de sessão
+  }),
+  secret: '123',//configure um segredo seu aqui
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var passport = require('passport');
+
+function authenticationMiddleware () {
+  return function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/login')
+  }
+}
+
+router.post('/login',
+  passport.authenticate('local', { successRedirect: '/chat', failureRedirect: '/login' })
+);
+
+router.get('/chat', authenticationMiddleware (), function(req, res){
+   res.render('chat', { username: req.user.username });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
